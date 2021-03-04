7a9c61a01709224c2f2fdcaad36b2467daeb96ac
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-03-03 20:37:52 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+// Create an App class component from scratch
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    //Write a componentDidMount method
+  componentDidMount() {
+      //Inside the method, send a fetch request to http://api.open-notify.org/astros.json
+    fetch('http://api.open-notify.org/astros.json')
+    .then(data => data.json())
+    .then(data => console.log("test"))
+  }
+
+  render() {
+    return "Test"
+  }
+}
